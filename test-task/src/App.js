e2fea82e3088add8f261cdae36c03678f0365904
Alex Martyan <alexanderthemartian@gmail.com>
2022-07-26 16:49:39 +0300

    refactor: clean some files

--- test-task/src/App.js
+++ test-task/src/App.js
@@ -2,7 +2,6 @@ import Header from "./components/Header/Header";
 import UserContainer from "./components/UsersContainer/UserContainer";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
 
-
 function App() {
   return (
     <>
